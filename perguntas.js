e316f6382574fcaa1a6c819dc0233c04c477fdfb
YasminCaires <cairesyasmin13@gmail.com>
2024-11-27 08:44:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Profissoes',
+    'Quantas profissões a Barbie já teve?',
+    'A Barbie já teve mais de 200 profissões, incluindo astronauta, médica, presidente, engenheira e jogadora de futebol.'
+)
+
+criaCartao(
+    'Namoro',
+    'Quem é o namorado da Barbie??',
+    'Ken Carson é o namorado da Barbie, apresentado em 1961.'
+)
+
+criaCartao(
+    'cinema',
+    'Quantos filmes da Barbie existem?',
+    'existem mais de 40 filmes animados da Barbie'
+)
+
+criaCartao(
+    'conhecimento',
+    'Quantos irmãos a Barbie tem?',
+    'A Barbie tem três irmãos: Skipper (irmã mais velha), Stacie (irmã do meio) e Chelsea (irmã mais nova)'
+)
